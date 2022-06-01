f038bfecdee71454fd7e3a255dfdc376fe1d9142
EmilioEscobedo1 <emilio@peer2peer.com.mx>
2022-05-31 19:48:46 -0500

    Modificaciones

--- 02_Variables/js/app.js
+++ 02_Variables/js/app.js
@@ -1,1 +1,9 @@
-//
+//Primer paso: Definir las variables
+let nombreCompleto; 
+let anioActual, anioNacimiento;
+let edad;
+
+//Segundo paso: Solicitar los datos
+nombreCompleto = prompt('Digita tu nombre completo');
+anioActual = prompt('Digita el año actual');
+anioNacimiento = prompt('Digita el año de tu nacimiento');
