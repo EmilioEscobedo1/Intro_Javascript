5b2b652b8088cde4b40fcde444420ebfc28a5e66
EmilioEscobedo1 <emilio@peer2peer.com.mx>
2022-06-07 19:22:53 -0500

    ejercicio_02

--- 06_Funciones/ejercicio_01.js
+++ 06_Funciones/ejercicio_01.js
@@ -1,12 +1,35 @@
 // La suma de dos valores
-const suma = function (primerNumero, segundoNumero) {
-    primerNumero + segundoNumero;
+const sumaDeDosValores = function (primerNumero, segundoNumero) {
+    return primerNumero + segundoNumero;
 };
 
-console.log(suma());
+console.log(suma(4, 4));
+console.log(suma(5, 8));
+console.log(suma(2, 1));
+console.log(suma(4, 5));
+console.log(suma(6, 7));
+console.log(suma(8, 9));
 
 // La resta de dos valores
+
+const resta = function (primerNumero, segundoNumero) {
+    return primerNumero - segundoNumero;
+};
+
+
 // La multiplicacion de dos valores
+
+const multiplicacion = function (primerNumero, segundoNumero) {
+    return primerNumero * segundoNumero;
+};
+
 // La division de dos valores
+
+const division = function (primerNumero, segundoNumero) {
+    return primerNumero / segundoNumero;
+};
+
 // El remanente de dos valores 
+
+
 // El exponente de dos valores
