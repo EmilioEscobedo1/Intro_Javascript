11d8bde3242a18721e2b5e4c196ef7d31d44287f
EmilioEscobedo1 <emilio@peer2peer.com.mx>
2022-06-07 20:57:46 -0500

    ejercicio_03

--- 06_Funciones/ejercicio_03/app.js
+++ 06_Funciones/ejercicio_03/app.js
@@ -1,3 +1,30 @@
-//Asignar valores
-let rojo = document.getElementById('red')
-rojo.style.backgroundColor='#b71c1c';
+function startTrafficSignal () {
+    let green=document.getElementById("verde");
+    let red=document.getElementById("rojo");
+    let yellow=document.getElementById("amarillo");
+
+    green.style.opacity=1;
+    setTimeout(function () {
+        green.style.opacity=.3;
+        red.style.opacity=.3;
+        yellow.style.opacity=1;
+    },5000);
+
+    setTimeout(function () {
+        green.style.opacity=.3;
+        red.style.opacity=1;
+        yellow.style.opacity=.3;
+    },7000);
+
+    setTimeout(function () {
+        green.style.opacity=1;
+        red.style.opacity=.3;
+        yellow.style.opacity=.3;
+    },12000);
+}
+
+let timer=setInterval(function () {
+    startTrafficSignal();
+},12000);
+
+startTrafficSignal()
