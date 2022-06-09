31d3bb4edea9f246017e6fc504613d761a1dd7db
EmilioEscobedo1 <emilio@peer2peer.com.mx>
2022-06-08 19:51:12 -0500

    objeto

---
+++ 08_Objetos/objeto.js
@@ -0,0 +1,22 @@
+const gato = {
+    nombre: "Alfa",
+    alias: "Peluchin",
+    apodo: "gato bola",
+    raza: "Siames",
+    edad: 6,
+    color: ["blanco", "gris"],
+    vacunas: {};
+};
+
+console.log(gato.edad);
+console.log(gato["edad"]);
+
+
+
+const miArray = ["string", 4, [], {}];
+miArray.length
+//arreglo de strings
+const razas = ["siames", "bengala", "carey", "persa", "bombay", "egipcio"];
+
+// arregloDeNumeros
+const pesos = [5,2,1,3,4];
