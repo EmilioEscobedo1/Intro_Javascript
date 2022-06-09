3fc3a6382464906ac6f32189ceb5203710866e39
EmilioEscobedo1 <emilio@peer2peer.com.mx>
2022-06-08 19:34:55 -0500

    scope

--- 07_Alcance/scope.js
+++ 07_Alcance/scope.js
@@ -7,12 +7,24 @@ const Hola = "Hola"
 let bonjour = "Bonjour"
 
 //funcion 
-function miFuncion() {
-    for (let index = 0; index < 5; index++){
-        if () {
+function saludar(nombre) {
+    console.log(`Hola ${nombre} desde el bloque de funcion`);
 
+    for (let index = 0; index < 5; index++){
+        console.log(index);
+        console.log(`Hola ${nombre} desde el for`);
+
+        if (index === 2) {
+            let genero = "M";
+            console.log(`Hola ${nombre}  desde el if`)
+            console.log(genero)
         }
     }
 }
 
 // bloque
+{}
+
+// if () {};
+// switch () {};
+// for () {};
