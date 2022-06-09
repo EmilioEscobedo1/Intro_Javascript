d329b3137f2524573999840e6c4a0d2ada8678a9
EmilioEscobedo1 <emilio@peer2peer.com.mx>
2022-06-08 19:22:32 -0500

    scope

---
+++ 07_Alcance/scope.js
@@ -0,0 +1,18 @@
+//global
+var hi = "Hi"
+
+
+// scope-bloque
+const Hola = "Hola"
+let bonjour = "Bonjour"
+
+//funcion 
+function miFuncion() {
+    for (let index = 0; index < 5; index++){
+        if () {
+
+        }
+    }
+}
+
+// bloque
