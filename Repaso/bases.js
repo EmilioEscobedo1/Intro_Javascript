15f9c9218c9024935ef7f6810ba25736b9d1f2d4
EmilioEscobedo1 <emilio@peer2peer.com.mx>
2022-06-14 20:29:49 -0500

    creacion

---
+++ Repaso/bases.js
@@ -0,0 +1,6 @@
+//Declarar una variable que se llame miEdad, y otra que se llame tuEdad y asignar un valor numerico a cada una
+
+const miEdad = 27;
+const tuEdad = 22;
+
+console.log(miEdad > tuEdad);
