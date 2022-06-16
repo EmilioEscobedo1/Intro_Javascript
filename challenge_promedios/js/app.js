63f87861024ad3eed269d69b7626fed5495d6619
EmilioEscobedo1 <emilio@peer2peer.com.mx>
2022-06-15 21:21:54 -0500

    casi funcional

--- challenge_promedios/js/app.js
+++ challenge_promedios/js/app.js
@@ -1,27 +1,28 @@
-let alumnos = document.getElementById('alumnos');
-let notas = document.getElementById('notas');
 let planilla = document.getElementById('planilla');
 
 function generar(){
-    
+    let alumnos = Number(document.getElementById('alumnos').value);
+    let notas = Number(document.getElementById('notas').value);
+
     //ciclo para crear filas
-    for (let i = 1; i<= (alumnos.value + 1); i++){
+    for(let i = 1; i <= alumnos + 1; i++){
         let tr = document.createElement('tr');
+
         //validamos si la fila a crear es el encabezado
         if(i===1){
             //ciclo para crear columnas
-            for (let j = 1; j <= (notas.value + 3); j++){
+            for(let j = 1; j <= notas + 3; j++){
                 let th = document.createElement('th');
                 let titulo;
 
-                if(j===1){
-                    titulo = document.createTextNode('Orden');
-                }else if (j===2){
+                if(j===1){ //esta es la columna de orden
+                     titulo = document.createTextNode('Orden');
+                }else if(j===2){ //columna de estudiante
                     titulo = document.createTextNode('Estudiante');
-                }else if (j===notas + 3){
+                }else if(j===notas + 3){ //columna de definitva
                     titulo = document.createTextNode('Def');
                 }else{
-                    titulo = document.createTextNode('Nota' + (j-2));
+                    titulo = document.createTextNode('Nota ' + (j - 2));
                 }
 
                 th.appendChild(titulo);
@@ -30,25 +31,24 @@ function generar(){
 
             planilla.appendChild(tr);
         }else{
-        
-                    for (let j = 1; j <= notas.value + 3; j++){
-                        let td = document.createElement('th');
-                        let elemento;
-                        let orden;
-        
-                        if(j===1){
-                            elemento = document.createTextNode(i - 1);
-                        }else if (j===notas + 3){
-                            elemento = document.createElement('span');
-                        }else{
-                            elemento = document.createElement('input');
-                        }
-        
-                        td.appendChild(elemento);
-                        tr.appendChild(td);
-                    }
-        
-                    planilla.appendChild(tr);
+            //ciclo para crear columnas
+            for(let j = 1; j <= notas + 3; j++){
+                let td = document.createElement('td');
+                let elemento;
+
+                if(j===1){ //esta es la columna de orden
+                     elemento = document.createTextNode(i - 1);
+                }else if(j===notas + 3){ //columna de definitva
+                    elemento = document.createElement('span');
+                }else{
+                    elemento = document.createElement('input');
+                }
+
+                td.appendChild(elemento);
+                tr.appendChild(td);
+            }
+
+            planilla.appendChild(tr);
         }
     }
 }
