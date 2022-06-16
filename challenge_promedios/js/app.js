466a2ed68c871dc7ad2a8cdb6430a5d24b1b64ad
EmilioEscobedo1 <emilio@peer2peer.com.mx>
2022-06-15 21:16:21 -0500

    reparaciones

--- challenge_promedios/js/app.js
+++ challenge_promedios/js/app.js
@@ -1,24 +1,54 @@
-function generarPlanilla() {
-    var body = document.getElementsByTagName('body')[0];
-    var tabla = document.createElement("table");
-    var tblBody = document.createElement("tbody");
+let alumnos = document.getElementById('alumnos');
+let notas = document.getElementById('notas');
+let planilla = document.getElementById('planilla');
 
-    for (var i = 0; i < 6; i++) {
+function generar(){
+    
+    //ciclo para crear filas
+    for (let i = 1; i<= (alumnos.value + 1); i++){
+        let tr = document.createElement('tr');
+        //validamos si la fila a crear es el encabezado
+        if(i===1){
+            //ciclo para crear columnas
+            for (let j = 1; j <= (notas.value + 3); j++){
+                let th = document.createElement('th');
+                let titulo;
 
-    var hilera = document.createElement("tr");
-    var textoHilera = document.createTextNode("Alumno"+i);
-    hilera.appendChild(textoHilera);
+                if(j===1){
+                    titulo = document.createTextNode('Orden');
+                }else if (j===2){
+                    titulo = document.createTextNode('Estudiante');
+                }else if (j===notas + 3){
+                    titulo = document.createTextNode('Def');
+                }else{
+                    titulo = document.createTextNode('Nota' + (j-2));
+                }
 
-    for (var j = 1; j < 6; j++) {  
-      var celda = document.createElement("td");
-      var textoCelda = document.createTextNode("Nota"+j);
-      celda.appendChild(textoCelda);
-      hilera.appendChild(celda);
+                th.appendChild(titulo);
+                tr.appendChild(th);
+            }
+
+            planilla.appendChild(tr);
+        }else{
+        
+                    for (let j = 1; j <= notas.value + 3; j++){
+                        let td = document.createElement('th');
+                        let elemento;
+                        let orden;
+        
+                        if(j===1){
+                            elemento = document.createTextNode(i - 1);
+                        }else if (j===notas + 3){
+                            elemento = document.createElement('span');
+                        }else{
+                            elemento = document.createElement('input');
+                        }
+        
+                        td.appendChild(elemento);
+                        tr.appendChild(td);
+                    }
+        
+                    planilla.appendChild(tr);
+        }
     }
-    tblBody.appendChild(hilera);
-  }
-    tabla.appendChild(tblBody);
-    body.appendChild(tabla);
-    tabla.setAttribute("border", "2");
-
 }
